6ec5475a26f0455f5767ea504bfd6b9620924597
Johnimony <ujif12349@yandex.ru>
2022-04-09 20:50:52 +0300

    test

--- main/tests.py
+++ main/tests.py
@@ -1,7 +1,12 @@
 from django.test import TestCase
-class URLTests(test.TestCase): 
+
+
+class URLTests(TestCase): 
+
+
     def test_homepage(self): 
         response = self.client.get('/') 
         self.assertEqual(response.status_code, 200)
 # Create your tests here.
 # Тесты
+# class URLTests(test.TestCase): def test_homepage(self): response = self.client.get('/') self.assertEqual(response.status_code, 200)
