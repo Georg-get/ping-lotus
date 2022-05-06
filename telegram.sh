5b9e87f82d632405eb4f1b44e41d128203d50c7f
Johnimony <ujif12349@yandex.ru>
2022-05-07 02:33:53 +0300

    telegram

--- telegram.sh
+++ telegram.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-curl -s -X POST https://api.telegram.org/bot'$TOKEN_ID'/sendMessage -d chat_id='$CHAT_ID' -d text="Джоба ${CI_JOB_ID}, ветки ${CI_COMMIT_BRANCH} ,успешно выполнена!" 
+
+TIME="10"
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+TEXT="Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+
+curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
