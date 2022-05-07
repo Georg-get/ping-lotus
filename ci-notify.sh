50bdbf13245ec3842fb85ea211d6f73259dbd8b0
Johnimony <ujif12349@yandex.ru>
2022-05-07 13:25:26 +0300

    telegram

--- ci-notify.sh
+++ ci-notify.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
-GROUP="chat_id=$TELEGRAM_GROUP_ID"
-
-curl -s -X POST $URL -d $GROUP -d text="Джоба ${CI_JOB_ID},  ветки ${CI_COMMIT_BRANCH} ,успешно выполнена" 
+curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage -d chat_id=$TELEGRAM_GROUP_ID -d text="Джоба ${CI_JOB_ID},  ветки ${CI_COMMIT_BRANCH} ,успешно выполнена"
